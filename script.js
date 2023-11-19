9c691c4c06de750243bbc65b136d0af5b9d68f53
gokayybaz <gokaybaz2000@gmail.com>
2023-11-19 15:17:14 +0300

    son

--- script.js
+++ script.js
@@ -1,53 +1,50 @@
-let instagramAddress = "https://www.instagram.com/gokayybaz/"
-let youtubeAddress = "https://www.youtube.com/channel/UCCaPG_zIeu_cxVdK0CUrmZw"
-let linkedInAddress = "https://www.linkedin.com/in/g%C3%B6kay-baz-233527250/"
-let twitterAddress = "https://twitter.com/gokayybaz1"
-let githubAddress = "https://github.com/gokayybaz"
+let instagramAddress = "https://www.instagram.com/gokayybaz/";
+let youtubeAddress = "https://www.youtube.com/channel/UCCaPG_zIeu_cxVdK0CUrmZw";
+let linkedInAddress = "https://www.linkedin.com/in/g%C3%B6kay-baz-233527250/";
+let twitterAddress = "https://twitter.com/gokayybaz1";
+let githubAddress = "https://github.com/gokayybaz";
 
-let instagramButton = document.getElementById("btn-instagram")
-let youtubeButton = document.getElementById("btn-youtube")
-let linkedInButton = document.getElementById("btn-linkedIn")
-let twitterButton = document.getElementById("btn-twitter")
-let githubButton = document.getElementById("btn-github")
+let instagramButton = document.getElementById("btn-instagram");
+let youtubeButton = document.getElementById("btn-youtube");
+let linkedInButton = document.getElementById("btn-linkedIn");
+let twitterButton = document.getElementById("btn-twitter");
+let githubButton = document.getElementById("btn-github");
 
-instagramButton.addEventListener("click",()=> {
-    open(instagramAddress, "_blank")
-})
+instagramButton.addEventListener("click", () => {
+  open(instagramAddress, "_blank");
+});
 
-youtubeButton.addEventListener("click",()=> {
-    open(youtubeAddress, "_blank")
-})
+youtubeButton.addEventListener("click", () => {
+  open(youtubeAddress, "_blank");
+});
 
-linkedInButton.addEventListener("click",()=> {
-    open(linkedInAddress, "_blank")
-})
+linkedInButton.addEventListener("click", () => {
+  open(linkedInAddress, "_blank");
+});
 
-twitterButton.addEventListener("click",()=> {
-    open(twitterAddress, "_blank")
-})
+twitterButton.addEventListener("click", () => {
+  open(twitterAddress, "_blank");
+});
 
-githubButton.addEventListener("click",()=> {
-    open(githubAddress, "_blank")
-})
+githubButton.addEventListener("click", () => {
+  open(githubAddress, "_blank");
+});
 
-let mobileNavShowButton = document.getElementById("btn-mobile-nav")
-let mobileNavArea = document.getElementById("mobile-nav")
-let mobileNavButtons = document.getElementsByClassName("nav--button")
-let mainBodyArea = document.getElementById("main-body")
-mainBodyArea.addEventListener("click",()=>{
-    mobileNavArea.style.display = "none"
+let mobileNavShowButton = document.getElementById("btn-mobile-nav");
+let mobileNavArea = document.getElementById("mobile-nav");
+let mobileNavButtons = document.getElementsByClassName("nav--button");
+let mainBodyArea = document.getElementById("main-body");
+mainBodyArea.addEventListener("click", () => {
+  mobileNavArea.style.display = "none";
+});
 
-})
-
-
-mobileNavShowButton.addEventListener("click",()=> {
-    mobileNavArea.style.display = "flex"
-})
+mobileNavShowButton.addEventListener("click", () => {
+  mobileNavArea.style.display = "flex";
+});
 
 for (let index = 0; index < mobileNavButtons.length; index++) {
-    let navButton = mobileNavButtons[index]
-    navButton.addEventListener("click",()=>{
-        mobileNavArea.style.display = "none"
-    })
-    
+  let navButton = mobileNavButtons[index];
+  navButton.addEventListener("click", () => {
+    mobileNavArea.style.display = "none";
+  });
 }
